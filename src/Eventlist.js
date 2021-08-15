49300c0e383bc1899407667808c2bd945886c7f1
Rosen Toshev <rtoscheff@gmail.com>
2021-08-15 18:26:41 +0100

    Added SwrHelper, isSuccess mutation, and cartAnimation.

--- src/Eventlist.js
+++ src/Eventlist.js
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
-import {useQuery} from 'react-query';
+import React from "react";
+import useSWR from 'swr';
 import Eventitem from "./Eventitem";
 import { getEvents } from "./EventHelper";
+import {fetcher} from './SwrHelper';
 
 export default function Eventlist() {
-  const [page, setPage] = useState(1)
-  const { isLoading, data } = useQuery(["events", page], () => getEvents(page))
+  const { data } = useSWR("http://localhost:3333/events", fetcher)
 
-  if (isLoading) {
+  if (!data) {
     return (
       <div className="container mt-5">Loading ...</div>
     )
@@ -28,32 +28,10 @@ export default function Eventlist() {
             </tr>
           </thead>
           <tbody>
-            {data.data.rows.map((event) => (
+            {data.map((event) => (
               <Eventitem event={event} key={event.id} />
             ))}
           </tbody>
-          <tfoot>
-            <tr>
-              <th colSpan="3" className="text-center">
-                <button
-                  className="btn btn-primary btn-primary-themed btn-md font-upper"
-                  disabled={page === 1}
-                  onClick={() => setPage(Math.max(1, page - 1))}
-                >
-                  Previous
-                </button>
-              </th>
-              <th colSpan="3">
-                <button
-                  className="btn btn-primary btn-primary-themed btn-md font-upper"
-                  disabled={!data.data.hasMore}
-                  onClick={() => setPage(page + 1)}
-                >
-                  Next
-                </button>
-              </th>
-            </tr>
-          </tfoot>
         </table>
       </div>
     </div>
